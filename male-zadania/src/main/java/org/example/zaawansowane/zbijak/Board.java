473c6988d0df954d0fd99e52e0ebd88f0444068c
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2023-08-31 11:52:00 +0200

    execute move input

--- male-zadania/src/main/java/org/example/zaawansowane/zbijak/Board.java
+++ male-zadania/src/main/java/org/example/zaawansowane/zbijak/Board.java
@@ -2,14 +2,21 @@ package org.example.zaawansowane.zbijak;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Board {
 
-    private List<Player> players = new ArrayList<>();
+    final private List<Player> players = new ArrayList<>();
+
+    public void start() {
+        createPlayer();
+        printBoard();
+        for (Player player : players) {
+            player.readMove();
+            printBoard();
+        }
+    }
 
     public void printBoard() {
-
         for (int y = 0; y < 10; y++) {
             for (int x = 0; x < 10; x++) {
                 boolean isPlayer = false;
@@ -41,31 +48,8 @@ public class Board {
         players.add(player4);
     }
 
-    public void readMove() {
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Chose your move: W, A, S, D");
-        switch (reader.next()) {
-            case "W":
-                System.out.println("Go up");
-                break;
-            case "S":
-                System.out.println("Go down");
-                break;
-            case "A":
-                System.out.println("Go to the left");
-                break;
-            case "D":
-                System.out.println("Go to the right");
-                break;
-            default:
-                System.out.println("Insert one of those move: W, A, S, D");
-        }
-    }
-
     public static void main(String[] args) {
         Board board = new Board();
-        board.createPlayer();
-        board.printBoard();
-        board.readMove();
+        board.start();
     }
 }
--- male-zadania/src/main/java/org/example/zaawansowane/zbijak/Player.java
+++ male-zadania/src/main/java/org/example/zaawansowane/zbijak/Player.java
@@ -1,5 +1,7 @@
 package org.example.zaawansowane.zbijak;
 
+import java.util.Scanner;
+
 public class Player {
 
     private String name;
@@ -11,6 +13,31 @@ public class Player {
         this.name = name;
         this.x = x;
         this.y = y;
+    }
+
+    public void readMove() {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Chose your move: W, A, S, D");
+        switch (reader.next()) {
+            case "W":
+                System.out.println("Go up");
+                y++;
+                break;
+            case "S":
+                System.out.println("Go down");
+                y--;
+                break;
+            case "A":
+                System.out.println("Go to the left");
+                x--;
+                break;
+            case "D":
+                System.out.println("Go to the right");
+                x++;
+                break;
+            default:
+                System.out.println("Insert one of those move: W, A, S, D");
+        }
     }
 
     public String getName() {
