73422f6ea682f05ce17a013488f72dd86dd3af44
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2023-10-11 14:44:14 +0200

    escapeRoom window action

--- male-zadania/src/main/java/org/example/advanced/escape_room/EscapeRoom.java
+++ male-zadania/src/main/java/org/example/advanced/escape_room/EscapeRoom.java
@@ -4,29 +4,59 @@ import java.util.Scanner;
 
 public class EscapeRoom {
 
+    public String input;
+    public Boolean window;
+    public Boolean door;
+    public Scanner scanner = new Scanner(System.in);
 
-    public void room() {
-        System.out.println("Hello you have to escape room. You have to find key and open the door.");
-        System.out.println("You have 3 items: window, key and door");
-        String key = "Key";
-        String window = "Window";
-        String door = "Door";
-        Scanner scanner = new Scanner(System.in);
-        do {
-            System.out.println("Choose item");
-            scanner.next();
+    public void start(){
+        room();
+        windowAction();
 
-
-        } while (true);
 
     }
+
+    public void windowAction(){
+        do {
+            input = scanner.next();
+            if (input.equals("close")) {
+                System.out.println("Window are closed");
+            } else if (input.equals("open")){
+                System.out.println("Window are open");
+            }
+        } while (true);
+    }
+
+
+    public void room() {
+        System.out.println("Hello you have to escape room. You have to find key and open the door. ");
+        System.out.print("You can open and close window using command 'open' and 'close'. ");
+        System.out.print("You can take key using command 'take'. ");
+        System.out.print("You can open door using command 'key'.");
+    }
+
+    public boolean executeInteraction() {
+        switch (input) {
+            case "key":
+                System.out.println("Key was taken");
+                break;
+            case "door":
+                System.out.println("Door are closed");
+                break;
+            case "window":
+                System.out.println("Window are opened");
+                break;
+            default:
+                System.out.println("Do action!");
+        }
+        return false;
+    }
+
+
 
 
     public static void main(String[] args) {
         EscapeRoom escapeRoom = new EscapeRoom();
-        escapeRoom.room();
-
-
+        escapeRoom.start();
     }
-
 }
