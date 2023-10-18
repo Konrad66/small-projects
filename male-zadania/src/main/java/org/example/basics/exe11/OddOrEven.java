4c7b242c5adda1bd3771a4015cf66e832f9175c0
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2023-10-19 00:09:25 +0200

    checking which number is biggest

--- male-zadania/src/main/java/org/example/basics/exe11/OddOrEven.java
+++ male-zadania/src/main/java/org/example/basics/exe11/OddOrEven.java
@@ -10,7 +10,7 @@ public class OddOrEven {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
 
-        if (number < 0 ){
+        if (number < 0) {
             System.out.println("number " + number + " is negative");
         } else if (number == 0) {
             System.out.println("number " + number + " is equals 0");
@@ -18,12 +18,26 @@ public class OddOrEven {
             System.out.println("number " + number + " is positive");
         }
 
-        if (number % 2 == 0){
+        if (number % 2 == 0) {
             System.out.println("number " + number + " is even");
         } else {
             System.out.println("number " + number + " is odd");
         }
 
+        System.out.println("Give me 3 different numbers: ");
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        int number3 = scanner.nextInt();
+
+        if (number1 > number2 && number3 < number1) {
+            System.out.println("Number " + number1 + " is the biggest ");
+        } else if (number2 > number1 && number3 < number2) {
+            System.out.println("Number " + number2 + " is the biggest ");
+        } else if (number3 > number1 && number3 > number2) {
+            System.out.println("Number " + number3 + " is the biggest ");
+        } else {
+            System.out.println("x");
+        }
 
     }
 }
