39122f851d41bdef43d3559a7b61d84e22000f3a
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2023-11-18 18:37:32 +0100

    add counter and draw again

--- male-zadania/src/main/java/org/example/basics/exe14/Loops.java
+++ male-zadania/src/main/java/org/example/basics/exe14/Loops.java
@@ -8,15 +8,19 @@ public class Loops {
 
         Random random = new Random();
         int numbers;
-
+        int counter = 0;
 
         while (true){
             numbers = random.nextInt(11);
-
+            counter++;
             System.out.println("You drew: " + numbers);
 
-            if(numbers == 10){
-                System.out.println("Congratulations, you drew 10");
+            if(numbers == 10) {
+                System.out.println("Congratulations, you drew 10. End of program after " + counter + " attempts.");
+                for (int i = 0; i < counter; i++) {
+                    int secondDraw = random.nextInt(counter);
+                    System.out.println("Again draw numbers: " + secondDraw);
+                }
                 break;
             }
         }
