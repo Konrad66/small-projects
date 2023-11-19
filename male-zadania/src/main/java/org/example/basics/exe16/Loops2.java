9637608973c2ab27df788d554da3a6bc7ff5dfcf
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2023-11-19 20:33:48 +0100

    Secret message

--- male-zadania/src/main/java/org/example/basics/exe16/Loops2.java
+++ male-zadania/src/main/java/org/example/basics/exe16/Loops2.java
@@ -1,6 +1,7 @@
 package org.example.basics.exe16;
 
 public class Loops2 {
+    //TODO do sprawdzenia
 
     public static void main(String[] args) {
         System.out.println("Numbers from 1 to 10");
---
+++ male-zadania/src/main/java/org/example/basics/exe17/SecretMessage.java
@@ -0,0 +1,23 @@
+package org.example.basics.exe17;
+
+import java.util.Scanner;
+
+public class SecretMessage {
+
+    public static void main(String[] args) {
+
+        System.out.println("Write password to see secret message.");
+        Scanner scanner = new Scanner(System.in);
+        String password;
+
+        while (true) {
+            System.out.println("Password: ");
+            password = scanner.next();
+            if(password.equals("1234")){
+                System.out.println("Hejka!");
+                break;
+            }
+        }
+
+    }
+}
