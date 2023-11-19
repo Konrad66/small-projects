6e707aa3e80fc56f33af09ee5af5aa88520a7c19
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2023-11-19 19:58:12 +0100

    loops2

--- male-zadania/src/main/java/org/example/basics/exe16/Loops2.java
+++ male-zadania/src/main/java/org/example/basics/exe16/Loops2.java
@@ -7,6 +7,20 @@ public class Loops2 {
         for(int i = 1; i < 11; i++){
             System.out.println(i);
         }
+        System.out.println("Number from 99 to 66");
+        for (int i = 99; i >= 66; i--){
+            System.out.println(i);
+        }
+        System.out.println("Even numbers from 20 to 40");
+        for(int i = 20; i <= 40; i++){
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+        }
 
+        String name = "Konrad";
+        for(int i = 0; i < 5; i++){
+            System.out.println(name);
+        }
     }
 }
