f76781a01fdf84641b610da4f61855c928c6f344
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2023-11-24 02:31:42 +0100

    is data ok

--- male-zadania/src/main/java/org/example/basics/exe13/BMI.java
+++ male-zadania/src/main/java/org/example/basics/exe13/BMI.java
@@ -9,15 +9,16 @@ public class BMI {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
         int age;
+        double growth;
+        double weight;
 
         do {
             System.out.println("How old are you?");
             age = scanner.nextInt();
             if (age >= 100) {
                 System.out.println("We don't give advice to people over 100 years old.");
-            } else if (age <= 0) {
+            } else if (age < 0) {
                 System.out.println("We do not provide advice for absurd values.");
             } else if (age < 18) {
                 System.out.println("Counseling for minors has a special clause.");
@@ -26,15 +27,9 @@ public class BMI {
             }
         } while (age < 0 || age > 100);
 
-
-
-
-        System.out.println("How tall are you? Enter in cm:");
-        double growth = scanner.nextDouble();
-        System.out.println("How much you weight? Enter in kg:");
-        double weight = scanner.nextDouble();
-
-        while (true) {
+        do {
+            System.out.println("How tall are you? Enter in cm:");
+            growth = scanner.nextDouble();
             if (growth < 30) {
                 System.out.println("We do not provide advice for absurd values.");
             } else if (growth > 254) {
@@ -42,10 +37,11 @@ public class BMI {
             } else {
                 System.out.println("Correct data");
             }
-            break;
-        }
+        } while (growth < 30 || growth > 254);
 
-        while (true) {
+        do {
+            System.out.println("How much you weight? Enter in kg:");
+            weight = scanner.nextDouble();
             if (weight < 1) {
                 System.out.println("We do not provide advice for absurd values.");
             } else if (weight > 595) {
@@ -53,9 +49,6 @@ public class BMI {
             } else {
                 System.out.println("Correct data");
             }
-            break;
-        }
-
-
+        } while (weight < 1 || weight > 595);
     }
 }
