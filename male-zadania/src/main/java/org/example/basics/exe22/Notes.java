eeea1e62db85ad600138477be630a7120d87379a
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2023-11-28 14:10:50 +0100

    notes

---
+++ male-zadania/src/main/java/org/example/basics/exe22/Notes.java
@@ -0,0 +1,28 @@
+package org.example.basics.exe22;
+
+import java.util.Scanner;
+
+public class Notes {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter number of notes: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        String[] notes = new String[number];
+
+        for (int i = 0; i < number; i++) {
+            System.out.print("Enter notes " + (i) + ": ");
+            notes[i] = scanner.nextLine();
+        }
+
+        System.out.println("All your notes: ");
+
+        for (String note : notes) {
+            System.out.println(note);
+        }
+
+        scanner.close();
+    }
+}
