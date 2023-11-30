3471dd1e99714c6ebd494ee534be69b812a1a626
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2023-12-01 00:34:18 +0100

    print Christmas tree

--- male-zadania/src/main/java/org/example/basics/exe27/Exercise27.java
+++ male-zadania/src/main/java/org/example/basics/exe27/Exercise27.java
@@ -6,6 +6,8 @@ public class Exercise27 {
 
     public static void main(String[] args) {
 
+        //TODO nie działa mi odwrócona choinka
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter the height of the Christmas tree: ");
@@ -13,7 +15,11 @@ public class Exercise27 {
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                if (j < (n - i) || j > (n + i)) {
+                    System.out.println("*");
+                } else {
+                    System.out.print("*");
+                }
             }
             System.out.println();
         }
