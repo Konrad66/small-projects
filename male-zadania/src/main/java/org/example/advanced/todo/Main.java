c2a91ce8e2c0604427cf9abd134eaa0e63eb2fe0
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2024-02-05 15:43:10 +0100

    plan ToDo

---
+++ male-zadania/src/main/java/org/example/advanced/todo/Main.java
@@ -0,0 +1,71 @@
+package org.example.advanced.todo;
+
+public class Main {
+
+    /*
+    Aplikacja ma pomóc w budowaniu nawyków i oznaczanie które zrobiliśmy
+
+    //możliwość dodania nawyku - nazwa, zrobiony/nie
+    //pokaze nawyki które dodaliśmy -> kolejny wybór jako nawyk skończony
+
+
+    1. Dodaj nawyk
+    2. Oznacz jak ci idzie - widzimy czy któreś są zrobione czy nie
+     */
+
+    /*
+    1. Dodaj nawyk
+    2. Oznacz jak ci idzie
+
+    1
+
+    Podaj nazwe:
+    Wypicie butelki wody
+
+    1. Dodaj nawyk
+    2. Oznacz jak ci idzie
+
+    1
+
+    Podaj nazwe:
+    przeczytałeś książke
+
+    1. Dodaj nawyk
+    2. Oznacz jak ci idzie
+
+    2
+
+    Twoje nawyki:
+    1. Wypicie butelki wody - NZ
+    2. przeczytałeś książke - NZ
+    3. Powrót do menu
+
+    2
+
+    Twoje nawyki:
+    1. Wypicie butelki wody - NZ
+    2. przeczytałeś książke - Z
+    3. Powrót do menu
+
+    3
+
+    1. Dodaj nawyk
+    2. Oznacz jak ci idzie
+
+    2
+
+    Twoje nawyki:
+    1. Wypicie butelki wody - NZ
+    2. przeczytałeś książke - Z
+    3. Powrót do menu
+
+    1
+
+    Twoje nawyki:
+    1. Wypicie butelki wody - Z
+    2. przeczytałeś książke - Z
+    3. Powrót do menu
+    Gratulacje zrobiłeś dziś wszystkie nawyki!
+     */
+
+}
