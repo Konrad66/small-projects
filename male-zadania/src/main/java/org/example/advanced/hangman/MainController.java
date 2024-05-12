a33c3a309d33f38354a4c2658b447c9a1e778f04
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2024-05-12 21:13:20 +0200

    split word

---
+++ male-zadania/src/main/java/org/example/advanced/hangman/MainController.java
@@ -0,0 +1,41 @@
+package org.example.advanced.hangman;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class MainController {
+
+
+    void program() {
+        String word = "komputer";
+        System.out.println("Zagrajmy w wisielca, musisz odgadnąc słowo:");
+        Scanner scanner = new Scanner(System.in);
+        String guess = scanner.next();
+        if (guess.equals(word)){
+            System.out.println("Brawo odgadłeś słowo");
+        } else {
+            System.out.println("Nie udało się :( Spróbuj następnym razem.");
+        }
+
+        int result = word.length();
+        System.out.println(result);
+
+        for (int i = 0; i < word.length(); i++){
+            char letter = word.charAt(i);
+            System.out.print(letter + "-");
+        }
+
+
+    }
+
+
+    //Rozlosuj
+
+
+    // 1. Graj
+    // 2. Tryb gry dwuosobowej - rywalizacja
+    // 3. Tryb gry dwuosobowej - na przemian
+    // 4. Wybierz słowo - użytkownik może wybrać długość słowa i takie zostanie wylosowane (int result = word.length();)
+    // 0. opuść program
+}
