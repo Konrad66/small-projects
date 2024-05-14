76b28f5a29032f13869b296b603f839c5221f731
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2024-05-14 12:20:24 +0200

    input control

---
+++ male-zadania/src/main/java/org/example/advanced/hangman/Input.java
@@ -0,0 +1,15 @@
+package org.example.advanced.hangman;
+
+import java.util.Scanner;
+
+public class Input {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    int readNumber(){
+        int number = scanner.nextInt();
+        scanner.nextLine();
+        return number;
+    }
+
+}
