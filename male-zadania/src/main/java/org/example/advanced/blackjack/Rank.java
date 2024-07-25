9e238a39bdf9ce6998c897886817be074655b173
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2024-07-25 17:36:49 +0200

    change power of ace

--- male-zadania/src/main/java/org/example/advanced/blackjack/Rank.java
+++ male-zadania/src/main/java/org/example/advanced/blackjack/Rank.java
@@ -2,7 +2,7 @@ package org.example.advanced.blackjack;
 
 public enum Rank {
 
-    TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(11), QUEEN(12), KING(13), ACE(14);
+    TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(10), QUEEN(10), KING(10), ACE(11);
 
     int power;
 
